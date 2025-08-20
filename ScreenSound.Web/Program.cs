c9b6f93ef72d017c15779e4f34739674fdbbd8d0
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-20 15:28:08 -0300

    Página artistas com listagem de nomes

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScreenSound.Web;
+using ScreenSound.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//ADICIONANDO SERVIÇO DE ArtistaAPI - CONSUMO DE Artistas da API
+builder.Services.AddTransient<ArtistaAPI>();
+
+//ADICIONANDO SERVIÇO DE HttpClient
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 await builder.Build().RunAsync();
---
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -0,0 +1,24 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services
+{
+    public class ArtistaAPI
+    {
+        //PROPRIEDADES - CAMPOS
+        private readonly HttpClient _httpClient;
+
+        //CONSTRUTOR
+        public ArtistaAPI(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("API");
+        }
+
+        //MÉTODO QUE RETORNA UMA COLEÇÃO DE ArtistaResponse DA API
+        public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+        {
+            return await
+                _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+        }
+    }
+}
