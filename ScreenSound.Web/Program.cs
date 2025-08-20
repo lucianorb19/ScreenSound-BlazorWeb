d12f46ba6d138a797e4ec717195d02fef06396ae
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-20 20:37:06 -0300

    Configurações Iniciais - MudBlazor

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
 
@@ -7,8 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
+
 //ADICIONANDO SERVIÇO DE ArtistaAPI - CONSUMO DE Artistas da API
 builder.Services.AddTransient<ArtistaAPI>();
+//builder.Services.AddTransient<MusicasAPI>();
 
 //ADICIONANDO SERVIÇO DE HttpClient
 builder.Services.AddHttpClient("API", client =>
