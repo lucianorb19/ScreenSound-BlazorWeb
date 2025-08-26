ed8338d853c35177fed10784c30d3f5bf287282f
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-26 11:23:01 -0300

    Preparação do projeto WEB para receber Musicas e Gêneros

---
+++ ScreenSound.Web/Services/GeneroAPI.cs
@@ -0,0 +1,29 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class GeneroAPI
+{
+    //PROPRIEDADES E CAMPOS
+    private readonly HttpClient _httpClient;
+
+
+    //CONSTRUTOR
+    public GeneroAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+
+    //DEMAIS MÉTODOS
+    public async Task<List<GeneroResponse>?> GetGenerosAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<GeneroResponse>>("generos");
+    }
+
+    public async Task<GeneroResponse?> GetGeneroPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<GeneroResponse>($"generos/{nome}");
+    }
+}
